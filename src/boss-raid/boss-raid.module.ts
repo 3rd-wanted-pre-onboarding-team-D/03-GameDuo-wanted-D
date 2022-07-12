00f2efa2c93e79ceda74112e8becc04da040680b
myeongil <username1103@naver.com>
2022-07-12 11:28:37 +0900

    Feat: redis cache setup

--- src/boss-raid/boss-raid.module.ts
+++ src/boss-raid/boss-raid.module.ts
@@ -1,12 +1,17 @@
 import { HttpModule } from '@nestjs/axios';
 import { CacheModule, Module } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
 import { TypeormModule } from 'typeorm.module';
 import { BossRaidController } from './boss-raid.controller';
 import { bossRaidProviders } from './boss-raid.providers';
 import { BossRaidService } from './boss-raid.service';
 
 @Module({
-  imports: [TypeormModule, CacheModule.register(), HttpModule],
+  imports: [
+    TypeormModule,
+    CacheModule.register({ store: redisStore, host: 'localhost', port: 6379 }),
+    HttpModule,
+  ],
   controllers: [BossRaidController],
   providers: [...bossRaidProviders, BossRaidService],
 })
